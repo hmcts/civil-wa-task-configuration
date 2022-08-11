1dd6b1d60a88d7918d39bfc61aac7f139a3bfa97
karthick mohanasundaram <92928238+karthick-mohanasundaram-HMCTS@users.noreply.github.com>
2022-08-11 09:10:53 +0100

    CIV-3664 Rename Judge makes decision

--- src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmnga/CamundaGaSubmissionTaskWaInitiationTest.java
+++ src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmnga/CamundaGaSubmissionTaskWaInitiationTest.java
@@ -464,7 +464,7 @@ public class CamundaGaSubmissionTaskWaInitiationTest extends DmnDecisionTableBas
             "generalAppUrgency", "Yes"
         ));
         VariableMap inputVariables = new VariableMapImpl();
-        inputVariables.putValue("eventId", "JUDGE_MAKES_DECISION");
+        inputVariables.putValue("eventId", "MAKE_DECISION");
         inputVariables.putValue("postEventState", "LISTING_FOR_A_HEARING");
         inputVariables.putValue("caseData", caseData);
         DmnDecisionTableResult dmnDecisionTableResult = evaluateDmnTable(inputVariables);
@@ -484,7 +484,7 @@ public class CamundaGaSubmissionTaskWaInitiationTest extends DmnDecisionTableBas
             "generalAppUrgency", "No"
         ));
         VariableMap inputVariables = new VariableMapImpl();
-        inputVariables.putValue("eventId", "JUDGE_MAKES_DECISION");
+        inputVariables.putValue("eventId", "MAKE_DECISION");
         inputVariables.putValue("postEventState", "LISTING_FOR_A_HEARING");
         inputVariables.putValue("caseData", caseData);
         DmnDecisionTableResult dmnDecisionTableResult = evaluateDmnTable(inputVariables);
