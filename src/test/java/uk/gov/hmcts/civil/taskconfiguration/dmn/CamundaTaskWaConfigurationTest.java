4ba9f67059a726efae6f36006f5f3c6c989ebbb8
skhajuria <sankhajuria@gmail.com>
2022-08-16 15:57:29 +0100

    CIV-3728 and CIV-3729 - Permissions and configuration DMN changes for Listing officer along with case management category.

--- src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaConfigurationTest.java
@@ -33,7 +33,7 @@ class CamundaTaskWaConfigurationTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(11));
+        assertThat(logic.getRules().size(), is(15));
     }
 
     @SuppressWarnings("checkstyle:indentation")
