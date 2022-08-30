e77bf000ebfad879c4d71951fa46826d3ad8b412
Chris Chandler <chris.chandler@hmcts.net>
2022-08-30 16:28:41 +0100

    Temp remove of test

--- src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaPermissionTest.java
+++ src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaPermissionTest.java
@@ -236,37 +236,6 @@ class CamundaTaskWaPermissionTest extends DmnDecisionTableBaseUnitTest {
 
     }
 
-//    @SuppressWarnings("checkstyle:indentation")
-//    @ParameterizedTest
-//    @CsvSource(value = {
-//        "LegalAdvisorSmallClaimsTrackDirections"
-//    })
-//    void given_legalAdv_taskType_when_evaluate_dmn_then_it_returns_expected_rule(String taskType) {
-//        VariableMap inputVariables = new VariableMapImpl();
-//        inputVariables.putValue("taskAttributes", Map.of("taskType", taskType));
-//
-//        DmnDecisionTableResult dmnDecisionTableResult = evaluateDmnTable(inputVariables);
-//
-//        MatcherAssert.assertThat(dmnDecisionTableResult.getResultList(), is(List.of(
-//            Map.of(
-//                "name", "tribunal-caseworker",
-//                "value", "Read,Own,Manage,Cancel",
-//                "roleCategory", "LEGAL_OPERATIONS"
-//            ),
-//            Map.of(
-//                "name", "hearing-centre-admin",
-//                "value", "Read,Own,Manage,Cancel",
-//                "roleCategory", "ADMIN"
-//            ),
-//            Map.of(
-//                "autoAssignable", false,
-//                "name", "task-supervisor",
-//                "value", "Read,Manage,Cancel"
-//            )
-//        )));
-//
-//    }
-
     @SuppressWarnings("checkstyle:indentation")
     @ParameterizedTest
     @CsvSource(value = {
