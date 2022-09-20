d4e1460efa6a0a7ddcb5d95e37eecfc8659f3566
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2022-09-20 14:46:01 +0100

    Run ft on camunda pipeline (#46)
    * Update Jenkinsfile_CNP
    
    * Update Jenkinsfile_CNP
    
    * update
    
    * update
    
    * update
    
    * Update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * Update camunda bpmn
    
    * Update camunda bpmn
    
    * update
    
    * update
    
    * update
    
    * update
    
    * Update
    
    * update
    
    * update
    
    * Update bpmn
    
    Co-authored-by: Mounika Ammineni <mounikaammineni@Mounikas-MacBook-Pro.local>

---
+++ bin/add-roles.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# User used during the CCD import and ccd-role creation
+./bin/utils/ccd-add-role.sh "caseworker-civil"
+./bin/utils/ccd-add-role.sh "caseworker-caa"
+./bin/utils/ccd-add-role.sh "caseworker-approver"
+./bin/utils/ccd-add-role.sh "prd-admin"
+./bin/utils/ccd-add-role.sh "judge-profile"
+./bin/utils/ccd-add-role.sh "basic-access" #to be removed after implementation of GS_profile
+./bin/utils/ccd-add-role.sh "GS_profile"
+./bin/utils/ccd-add-role.sh "legal-adviser"
+./bin/utils/ccd-add-role.sh "civil-administrator-standard"
+./bin/utils/ccd-add-role.sh "civil-administrator-basic"
+./bin/utils/ccd-add-role.sh "caseworker-ras-validation"
+./bin/utils/ccd-add-role.sh "admin-access"
+./bin/utils/ccd-add-role.sh "full-access"
+
+roles=("solicitor" "systemupdate" "admin" "staff")
+for role in "${roles[@]}"
+do
+  ./bin/utils/ccd-add-role.sh "caseworker-civil-${role}"
+done
