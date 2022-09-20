7347dda7d83018959fc8f22a4289e236e34a9200
Mounika Ammineni <mounikaammineni@Mounikas-MacBook-Pro.local>
2022-09-20 14:19:38 +0100

    Update bpmn

--- bin/import-dmn-diagram.sh
+++ bin/import-dmn-diagram.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -ex
+set -eu
 workspace=${1}
 env=${2}
 
@@ -12,16 +12,10 @@ fi
 
 serviceToken=$($(realpath $workspace)/bin/utils/idam-lease-service-token.sh civil_service \
   $(docker run --rm toolbelt/oathtool --totp -b ${s2sSecret}))
-dmnFilepath="$(realpath $workspace)/src/main/resources"
-bpmnFilepath="$(realpath $workspace)/camunda"
-if [ -d ${bpmnFilepath} ]
-then
-  allFiles=$(find ${dmnFilepath} -name '*.dmn')+$(find ${bpmnFilepath} -name '*.bpmn')
-else
-  allFiles=$(find ${dmnFilepath} -name '*.dmn')
-fi
 
-for file in ${allFiles}
+dmnFilepath="$(realpath $workspace)/src/main/resources"
+
+for file in $(find ${dmnFilepath} -name '*.dmn')
 do
   uploadResponse=$(curl --insecure -v --silent -w "\n%{http_code}" --show-error -X POST \
     ${CAMUNDA_BASE_URL:-http://localhost:9404}/engine-rest/deployment/create \
@@ -29,7 +23,7 @@ do
     -H "ServiceAuthorization: Bearer ${serviceToken}" \
     -F "deployment-name=$(basename ${file})" \
     -F "deploy-changed-only=true" \
-    -F "file=@${allFiles}/$(basename ${file})")
+    -F "file=@${dmnFilepath}/$(basename ${file})")
 
 upload_http_code=$(echo "$uploadResponse" | tail -n1)
 upload_response_content=$(echo "$uploadResponse" | sed '$d')
@@ -43,4 +37,33 @@ echo "$(basename ${file}) upload failed with http code ${upload_http_code} and r
 continue;
 
 done
-exit 0;
+
+bpmnFilepath="$(realpath $workspace)/camunda"
+if [ -d ${bpmnFilepath} ]
+then
+  for file in $(find ${bpmnFilepath} -name '*.bpmn')
+  do
+    uploadResponse=$(curl --insecure -v --silent -w "\n%{http_code}" --show-error -X POST \
+      ${CAMUNDA_BASE_URL:-http://localhost:9404}/engine-rest/deployment/create \
+      -H "Accept: application/json" \
+      -H "ServiceAuthorization: Bearer ${serviceToken}" \
+      -F "deployment-name=$(basename ${file})" \
+      -F "deploy-changed-only=true" \
+      -F "file=@${bpmnFilepath}/$(basename ${file})")
+
+  upload_http_code=$(echo "$uploadResponse" | tail -n1)
+  upload_response_content=$(echo "$uploadResponse" | sed '$d')
+
+  if [[ "${upload_http_code}" == '200' ]]; then
+    echo "$(basename ${file}) diagram uploaded successfully (${upload_response_content})"
+    continue;
+  fi
+
+  echo "$(basename ${file}) upload failed with http code ${upload_http_code} and response (${upload_response_content})"
+  continue;
+
+  done
+  exit 0; 
+fi
+
+
