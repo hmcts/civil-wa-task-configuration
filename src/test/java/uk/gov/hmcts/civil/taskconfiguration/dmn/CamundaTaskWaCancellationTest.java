26ab172b134533890d840646ed8b631e27b07897
Nagaraju Deekonda <nagaraju.deekonda@version1.com>
2022-10-15 12:44:01 +0100

    civil-5332-config-changes-to-cancellation-dmn

--- src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaCancellationTest.java
+++ src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaCancellationTest.java
@@ -26,7 +26,7 @@ class CamundaTaskWaCancellationTest extends DmnDecisionTableBaseUnitTest {
     }
 
     @ParameterizedTest
-    @MethodSource({"scenarioProvider", "scenarioProviderSdo", "scenarioProviderSdo1"})
+    @MethodSource("scenarioProvider")
     void given_multiple_event_ids_should_evaluate_dmn(String fromState,
                                                       String eventId,
                                                       String state,
@@ -46,6 +46,10 @@ class CamundaTaskWaCancellationTest extends DmnDecisionTableBaseUnitTest {
             Map.of(
                 "action", "Cancel",
                 "processCategories", "defaultJudgment"
+            ),
+            Map.of(
+                "action", "Cancel",
+                "processCategories", "standardDirectionsOrder"
             )
         );
         return Stream.of(
@@ -65,60 +69,12 @@ class CamundaTaskWaCancellationTest extends DmnDecisionTableBaseUnitTest {
         );
     }
 
-    public static Stream<Arguments> scenarioProviderSdo() {
-        List<Map<String, String>> outcome = List.of(
-            Map.of(
-                "action", "Cancel",
-                "processCategories", "standardDirectionsOrder"
-            )
-        );
-        return Stream.of(
-            Arguments.of(
-                "any state", "TAKE_CASE_OFFLINE", "PROCEEDS_IN_HERITAGE_SYSTEM",
-                outcome
-            ),
-            Arguments.of(
-                "", "TAKE_CASE_OFFLINE", "PROCEEDS_IN_HERITAGE_SYSTEM",
-                outcome
-            ),
-            Arguments.of(
-                null, "TAKE_CASE_OFFLINE", "PROCEEDS_IN_HERITAGE_SYSTEM",
-                outcome
-
-            )
-        );
-    }
-
-    public static Stream<Arguments> scenarioProviderSdo1() {
-        List<Map<String, String>> outcome = List.of(
-            Map.of(
-                "action", "Reconfigure",
-                "processCategories", "standardDirectionsOrder"
-            )
-        );
-        return Stream.of(
-            Arguments.of(
-                "any state", "CLAIMANT_RESPONSE", "any state",
-                outcome
-            ),
-            Arguments.of(
-                "any state", "CLAIMANT_RESPONSE", "",
-                outcome
-            ),
-            Arguments.of(
-                "any state", "CLAIMANT_RESPONSE", null,
-                outcome
-
-            )
-        );
-    }
-
     @Test
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(3));
         assertThat(logic.getOutputs().size(), is(4));
-        assertThat(logic.getRules().size(), is(4));
+        assertThat(logic.getRules().size(), is(2));
     }
 }
