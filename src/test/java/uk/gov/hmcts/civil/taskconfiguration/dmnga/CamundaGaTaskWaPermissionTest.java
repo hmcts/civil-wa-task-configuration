077a4d4c0bdcc1a2d35b9e655a6adacb847c8249
Deepthi Doppalapudi <deepthi.doppalapudi@version1.com>
2022-10-19 08:16:34 +0100

    specificaccesspermission tests added
    removed spaces in Permissions DMN - reviewSpecificAccessRequestJudiciary
    Added test case for the task type "reviewSpecificAccessRequestJudiciary"

--- src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmnga/CamundaGaTaskWaPermissionTest.java
+++ src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmnga/CamundaGaTaskWaPermissionTest.java
@@ -279,4 +279,32 @@ public class CamundaGaTaskWaPermissionTest extends DmnDecisionTableBaseUnitTest 
             )
         )));
     }
+
+    @SuppressWarnings("checkstyle:indentation")
+    @ParameterizedTest
+    @CsvSource(value = {
+        "reviewSpecificAccessRequestJudiciary"
+    })
+    void ReviewSpecificAccessRequestJudiciaryTaskType_when_evaluate_dmn_then_it_returns_expected_rule(String taskType) {
+        VariableMap inputVariables = new VariableMapImpl();
+        inputVariables.putValue("taskAttributes", Map.of("taskType", taskType));
+
+        DmnDecisionTableResult dmnDecisionTableResult = evaluateDmnTable(inputVariables);
+
+        MatcherAssert.assertThat(dmnDecisionTableResult.getResultList(), is(List.of(
+            Map.of(
+                "name", "task-supervisor",
+                "value", "Read,Manage,Cancel",
+                "autoAssignable", false
+            ),
+            Map.of(
+                "name", "leadership-judge",
+                "value", "Read,Manage,Own,Cancel",
+                "roleCategory", "JUDICIAL",
+                "authorisations", "294",
+                "assignmentPriority", 1,
+                "autoAssignable", true
+            )
+        )));
+    }
 }
