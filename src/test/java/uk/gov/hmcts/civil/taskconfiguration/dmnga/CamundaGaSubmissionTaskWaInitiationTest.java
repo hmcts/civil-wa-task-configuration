d4fb459a55afdbc79307fe0de25472b5fab21c15
Deepthi Doppalapudi <deepthi.doppalapudi@version1.com>
2022-11-23 13:29:35 +0000

    CIV-6196 Added before sdo scenario for List for Hearing cases

--- src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmnga/CamundaGaSubmissionTaskWaInitiationTest.java
+++ src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmnga/CamundaGaSubmissionTaskWaInitiationTest.java
@@ -510,6 +510,7 @@ public class CamundaGaSubmissionTaskWaInitiationTest extends DmnDecisionTableBas
         data.put("generalAppUrgencyRequirement", Map.of(
             "generalAppUrgency", true
         ));
+        data.put("isCcmccLocation", false);
         Map<String, Object> caseData = new HashMap<>();
         caseData.put("Data", data);
 
@@ -533,6 +534,7 @@ public class CamundaGaSubmissionTaskWaInitiationTest extends DmnDecisionTableBas
         data.put("generalAppUrgencyRequirement", Map.of(
             "generalAppUrgency", false
         ));
+        data.put("isCcmccLocation", true);
         Map<String, Object> caseData = new HashMap<>();
         caseData.put("Data", data);
 
