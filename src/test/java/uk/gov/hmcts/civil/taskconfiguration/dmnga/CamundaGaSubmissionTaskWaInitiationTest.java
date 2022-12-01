76b3b216895099ea8aad346feb7b22b97e25df24
Deepthi Doppalapudi <deepthi.doppalapudi@version1.com>
2022-12-01 13:18:50 +0000

    Revert "Merge branch 'master' into feature/CIV-5167"
    This reverts commit a59fd240e83d287136d7fa6f74b48bad084a1beb, reversing
    changes made to 0f90366ffdcd872f93a60d5bdde8b31cf5c2f2a6.

--- src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmnga/CamundaGaSubmissionTaskWaInitiationTest.java
+++ src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmnga/CamundaGaSubmissionTaskWaInitiationTest.java
@@ -1000,7 +1000,6 @@ public class CamundaGaSubmissionTaskWaInitiationTest extends DmnDecisionTableBas
         data.put("generalAppUrgencyRequirement", Map.of(
             "generalAppUrgency", true
         ));
-        data.put("isCcmccLocation", false);
         Map<String, Object> caseData = new HashMap<>();
         caseData.put("Data", data);
 
@@ -1024,7 +1023,6 @@ public class CamundaGaSubmissionTaskWaInitiationTest extends DmnDecisionTableBas
         data.put("generalAppUrgencyRequirement", Map.of(
             "generalAppUrgency", false
         ));
-        data.put("isCcmccLocation", true);
         Map<String, Object> caseData = new HashMap<>();
         caseData.put("Data", data);
 
