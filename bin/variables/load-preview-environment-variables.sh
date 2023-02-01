c0043e7afc5c2f6ed36e71606f9fc791725ffc9e
skhajuria <sankhajuria@gmail.com>
2023-02-01 17:32:32 +0000

    updated the dependency check verion

--- bin/variables/load-preview-environment-variables.sh
+++ bin/variables/load-preview-environment-variables.sh
@@ -7,7 +7,7 @@ pr=${1}
 echo 'export ENVIRONMENT=preview'
 
 # urls
-echo "export SERVICE_AUTH_PROVIDER_API_BASE_URL=http://rpe-service-auth-provider-aat.aat.platform.hmcts.net"
+echo "export SERVICE_AUTH_PROVIDER_API_BASE_URL=http://rpe-service-auth-provider-aat.service.core-compute-aat.internal"
 echo "export IDAM_API_BASE_URL=https://idam-api.aat.platform.hmcts.net"
 echo "export CCD_IDAM_REDIRECT_URL=https://ccd-case-management-web-aat.aat.platform.hmcts.net/oauth2redirect"
 echo "export CCD_DEFINITION_STORE_API_BASE_URL=https://ccd-definition-store-civil-task-configuration-pr-${pr}.preview.platform.hmcts.net"
