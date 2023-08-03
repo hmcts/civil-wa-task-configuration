9401feba10c1ca9bb97598cfb08f4d6a306c2d7d
neeta-hmcts <115545612+neeta-hmcts@users.noreply.github.com>
2023-08-03 12:54:12 +0100

    Update add-roles.sh

--- bin/add-roles.sh
+++ bin/add-roles.sh
@@ -31,6 +31,7 @@
 ./bin/utils/ccd-add-role.sh "CITIZEN-DEFENDANT-PROFILE"
 ./bin/utils/ccd-add-role.sh "cui-admin-profile"
 ./bin/utils/ccd-add-role.sh "cui-nbc-profile"
+./bin/utils/ccd-add-role.sh "citizen-profile"
 
 roles=("solicitor" "systemupdate" "admin" "staff")
 for role in "${roles[@]}"
