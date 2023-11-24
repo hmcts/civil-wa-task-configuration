769a1f7bd455acdf6e6df91c06fd7aa7a2716acb
UshaPanneerselvam1 <Usha.Panneerselvam@HMCTS.NET>
2023-11-24 17:00:27 +0000

    CIV-11501 Unit test fix

--- src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskCompletionTest.java
@@ -217,7 +217,7 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
     @Test
     void if_this_test_fails_needs_updating_with_your_changes() {
 
-        //The purpose of this test is to prevent adding new rows without being tested
+        //Test :The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getRules().size(), is(23));
     }
