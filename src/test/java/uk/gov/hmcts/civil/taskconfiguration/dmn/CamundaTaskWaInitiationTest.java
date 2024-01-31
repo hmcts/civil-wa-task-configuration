8899767858b080176d517786d08e649c96b5e64e
jeswanth <jeswanth.reddy@hmcts.net>
2024-01-31 14:52:38 +0000

    CIV-11768 configured for creating the wa tasks for case flags

--- src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaInitiationTest.java
+++ src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaInitiationTest.java
@@ -666,11 +666,34 @@ class CamundaTaskWaInitiationTest extends DmnDecisionTableBaseUnitTest {
                        .get(0).get("taskId"), is("claimantWelshRequest"));
         assertThat(workTypeResultList.get(0).get("processCategories"), is("requestTranslation"));
     }
+    @Test
+    void given_input_should_return_review_case_flag_for_claimant_during_support_needs() {
+        Map<String, Object> data = new HashMap<>();
+        data.put("applicant1DQVulnerabilityQuestions",Map.of("vulnerabilityAdjustmentsRequired",true));
+        data.put("applicant1DQLanguage",Map.of( "court","BOTH"));
+        data.put("featureToggleWA", "CUIR2");
 
+        Map<String, Object> caseData = new HashMap<>();
+        caseData.put("Data", data);
+
+        VariableMap inputVariables = new VariableMapImpl();
+        inputVariables.putValue("eventId", "CLAIMANT_RESPONSE_CUI");
+        inputVariables.putValue("additionalData", caseData);
+
+        DmnDecisionTableResult dmnDecisionTableResult = evaluateDmnTable(inputVariables);
+
+        List<Map<String, Object>> workTypeResultList = dmnDecisionTableResult.getResultList();
+
+        assertThat(workTypeResultList.size(), is(2));
+        assertThat(workTypeResultList
+                       .get(0).get("taskId"), is("ReviewCaseFlagsForClaimant"));
+        assertThat(workTypeResultList
+                       .get(0).get("name"), is("Review Case Flags Claimant"));
+    }
     @Test
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(124));
+        assertThat(logic.getRules().size(), is(129));
     }
 }
