685ee17c79f91281f2afe6ff1d1b3b631bea6fe5
meulendijksm <max.meulendijks@hmcts.net>
2024-02-02 14:08:05 +0000

    CIV-11297 - Added input + completion event

--- src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskCompletionTest.java
@@ -189,6 +189,10 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
                     Map.of(
                         "taskType", "UpdateDetailsInCasemanSystem",
                         "completionMode", "Auto"
+                    ),
+                    Map.of(
+                        "taskType", "HelpWithFeesHearingFee",
+                        "completionMode", "Auto"
                     )
                 )
             )
@@ -320,6 +324,6 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
 
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(35));
+        assertThat(logic.getRules().size(), is(36));
     }
 }
