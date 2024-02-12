66b1b0465a6f8d22f4711443db5a5b51162f51ab
ShwetaTandel-hmcts <shweta.tandel@hmcts.net>
2024-02-12 09:50:00 +0000

    merge changes from master

--- src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaInitiationTest.java
+++ src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaInitiationTest.java
@@ -939,7 +939,7 @@ class CamundaTaskWaInitiationTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(200));
+        assertThat(logic.getRules().size(), is(201));
     }
 
     @Test
