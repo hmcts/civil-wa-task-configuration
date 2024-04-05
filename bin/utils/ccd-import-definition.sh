4048d2b4ef551c8ff60077551c2b48392e691d7a
drummondjm <93932689+drummondjm@users.noreply.github.com>
2024-04-05 10:29:53 +0100

    CIV-13927 Updated SLA of trial readiness check (#548)
    * Updated SLA of trial readiness check ( preHearingContact )
    
    * Updated SLA of trial readiness check ( preHearingContact )
    
    * few tweaks on upload script
    
    * Update ccd-import-definition.sh
    
    * Update ccd-import-definition.sh
    
    * Update ccd-import-definition.sh
    
    * Update ccd-import-definition.sh
    
    * changing to yq image
    
    ---------
    
    Co-authored-by: vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>

--- bin/utils/ccd-import-definition.sh
+++ bin/utils/ccd-import-definition.sh
@@ -9,10 +9,25 @@ filepath=${1}
 filename=$(basename ${filepath})
 uploadFilename="$(date +"%Y%m%d-%H%M%S")-${filename}"
 
+echo "Getting userToken and serviceToken"
 userToken=$(${dir}/idam-lease-user-token.sh ${CCD_CONFIGURER_IMPORTER_USERNAME:-ccd.docker.default@hmcts.net} ${CCD_CONFIGURER_IMPORTER_PASSWORD:-Password12!})
+echo ">>> Got userToken"
 serviceToken=$(${dir}/idam-lease-service-token.sh ccd_gw $(docker run --rm toolbelt/oathtool --totp -b ${CCD_API_GATEWAY_S2S_SECRET:-AAAAAAAAAAAAAAAC}))
+echo ">>> Got serviceToken"
 
-uploadResponse=$(curl --insecure --silent -w "\n%{http_code}" --show-error -X POST \
+version="n/a"
+newVersion="n/a"
+
+if [[ "${ENVIRONMENT}" == "preview" ]]; then
+  version=$(curl --insecure --silent --show-error -X GET \
+    ${CCD_DEFINITION_STORE_API_BASE_URL:-http://localhost:4451}/api/data/case-type/CIVIL/version \
+    -H "Authorization: Bearer ${userToken}" \
+    -H "ServiceAuthorization: Bearer ${serviceToken}" || echo 'bypass-if-error')
+
+  echo "Current version is ${version}"
+fi
+
+uploadResponse=$(curl --insecure --silent -w "\n%{http_code}" --show-error --max-time 60 -X POST \
   ${CCD_DEFINITION_STORE_API_BASE_URL:-http://localhost:4451}/import \
   -H "Authorization: Bearer ${userToken}" \
   -H "ServiceAuthorization: Bearer ${serviceToken}" \
@@ -21,8 +36,27 @@ uploadResponse=$(curl --insecure --silent -w "\n%{http_code}" --show-error -X PO
 upload_http_code=$(echo "$uploadResponse" | tail -n1)
 upload_response_content=$(echo "$uploadResponse" | sed '$d')
 
+if [ "${ENVIRONMENT}" == "preview" ] && [ "${upload_http_code}" != "201" ]; then
+  echo "Bypassing audit check as on preview - will wait 45s and then verify the version has changed"
+  sleep 45
+
+  newVersion=$(curl --insecure --silent --show-error -X GET \
+    ${CCD_DEFINITION_STORE_API_BASE_URL:-http://localhost:4451}/api/data/case-type/CIVIL/version \
+    -H "Authorization: Bearer ${userToken}" \
+    -H "ServiceAuthorization: Bearer ${serviceToken}" || echo 'bypass-if-error')
+
+    echo "Current version is ${newVersion}"
+    if [[ "$newVersion" == "$version" ]]; then
+      echo "Version has not changed - the definition was not imported successfully"
+      exit 1
+    fi
+
+  echo "CCD definition version has changed, definition successfully uploaded"
+  exit 0
+fi
+
 if [[ "${upload_http_code}" == '504' ]]; then
-  for try in {1..10}
+  for try in {1..20}
   do
     sleep 5
     echo "Checking status of ${filename} (${uploadFilename}) upload (Try ${try})"
