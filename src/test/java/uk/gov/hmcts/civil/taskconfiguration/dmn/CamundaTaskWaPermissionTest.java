bbc204bc5779edea33c0d9513a5152b4ef23f0af
hmcts-version1-ignacio <ignacio.mendizabal@version1.com>
2024-06-17 15:07:33 +0100

    CIV-14016 Fix checkstyle error

--- src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaPermissionTest.java
+++ src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaPermissionTest.java
@@ -887,7 +887,7 @@ class CamundaTaskWaPermissionTest extends DmnDecisionTableBaseUnitTest {
     @CsvSource(value = {
         "ClaimSettledDivergenceTakeCaseOffline"
     })
-    void given_ClaimSettledDivergenceTakeCaseOffline_taskType_when_evaluate_dmn_then_returns_expected_rule(String taskType) {
+    void given_ClaimDivergenceTakeCaseOffline_taskType_when_evaluate_dmn_then_returns_expected_rule(String taskType) {
         VariableMap inputVariables = new VariableMapImpl();
         inputVariables.putValue("taskAttributes", Map.of("taskType", taskType));
         inputVariables.putValue("caseData",Map.of("featureToggleWA", "SD"));
