f276c338c36289f76979c69ccee602b2850fec26
Omaira-Melo-Hmcts <148855113+Omaira-Melo-Hmcts@users.noreply.github.com>
2024-06-28 09:27:20 +0100

    CIV-14129 WA for SETTLE_CLAIM_MARK_PAID_FULL

--- src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaCancellationTest.java
+++ src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaCancellationTest.java
@@ -363,6 +363,6 @@ class CamundaTaskWaCancellationTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(3));
         assertThat(logic.getOutputs().size(), is(4));
-        assertThat(logic.getRules().size(), is(30));
+        assertThat(logic.getRules().size(), is(31));
     }
 }
