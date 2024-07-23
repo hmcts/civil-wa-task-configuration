2857ae8fb10b643835e1dd7fa0f05dbb1f56b550
sankaviv1 <95748224+sankaviv1@users.noreply.github.com>
2024-07-23 15:03:42 +0100

    CIV-14741 Remove completion for schedule a hearing (#638)
    * CIV-14741 Remove completion for schedule a hearing
    
    * fix test
    
    * fix test
    
    * revert exporter version

--- src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskCompletionTest.java
@@ -183,10 +183,6 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
                         "completionMode", "Auto"
                     ),
                     Map.of(
-                        "taskType", "ScheduleAHearing",
-                        "completionMode", "Auto"
-                    ),
-                    Map.of(
                         "taskType", "UpdateDetailsInCasemanSystem",
                         "completionMode", "Auto"
                     ),
@@ -392,6 +388,6 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
 
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(40));
+        assertThat(logic.getRules().size(), is(39));
     }
 }
