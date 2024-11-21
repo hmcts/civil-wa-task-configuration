26c8d21750bee8dd23059e6ba4a9db79a4b2845b
VijayaKumarK <kannan.vijayakumar@hmcts.net>
2024-11-21 17:19:04 +0000

    CIV-15663 update tests

--- src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaConfigurationTest.java
@@ -37,7 +37,7 @@ class CamundaTaskWaConfigurationTest extends DmnDecisionTableBaseUnitTest {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
 
-        assertThat(logic.getRules().size(), is(114));
+        assertThat(logic.getRules().size(), is(115));
     }
 
     @SuppressWarnings("checkstyle:indentation")
