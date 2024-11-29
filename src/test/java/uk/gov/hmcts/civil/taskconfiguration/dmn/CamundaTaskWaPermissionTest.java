b8b98c8e7b5572a11637c708d16c011f770ac611
VijayaKumarK <kannan.vijayakumar@hmcts.net>
2024-11-29 11:47:56 +0000

    CIV-15663 checkstyle

--- src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaPermissionTest.java
+++ src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaPermissionTest.java
@@ -1074,7 +1074,7 @@ class CamundaTaskWaPermissionTest extends DmnDecisionTableBaseUnitTest {
     void given_JoSetAsideTakeCaseOffline_taskType_when_evaluate_dmn_then_return_expected_rule(String taskType) {
         VariableMap inputVariables = new VariableMapImpl();
         inputVariables.putValue("taskAttributes", Map.of("taskType", taskType));
-            inputVariables.putValue("caseData",Map.of("featureToggleWA", "JO"));
+        inputVariables.putValue("caseData",Map.of("featureToggleWA", "JO"));
         DmnDecisionTableResult dmnDecisionTableResult = evaluateDmnTable(inputVariables);
 
         MatcherAssert.assertThat(dmnDecisionTableResult.getResultList(), is(List.of(
