8af18d9d1992f9af579ed334b24826528a671c6f
drummondjm <mark.drummond@version1.com>
2024-12-06 12:51:02 +0000

    fix duplicate location in config

--- src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaConfigurationTest.java
@@ -37,7 +37,7 @@ class CamundaTaskWaConfigurationTest extends DmnDecisionTableBaseUnitTest {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
 
-        assertThat(logic.getRules().size(), is(127));
+        assertThat(logic.getRules().size(), is(125));
     }
 
     @SuppressWarnings("checkstyle:indentation")
