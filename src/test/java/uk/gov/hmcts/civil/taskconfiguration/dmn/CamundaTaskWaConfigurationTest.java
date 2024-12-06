a951e89c189ed359abb12ba53c59f3df0bc1d468
meulendijksm <max.meulendijks@hmcts.net>
2024-12-06 11:16:02 +0000

    CIV-15393 - fix tab name

--- src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaConfigurationTest.java
@@ -2124,25 +2124,25 @@ class CamundaTaskWaConfigurationTest extends DmnDecisionTableBaseUnitTest {
     @ParameterizedTest
     @CsvSource(value = {
         "reviewMessageCW; Small Claim;; "
-            + "[Small Claim, CW, Review Message](/cases/case-details/${[CASE_REFERENCE]}#messagesTab); Yes",
+            + "[Small Claim, CW, Review Message](/cases/case-details/${[CASE_REFERENCE]}#Messages); Yes",
         "reviewMessageCW; Fast Track;; "
-            + "[Fast Track, CW, Review Message](/cases/case-details/${[CASE_REFERENCE]}#messagesTab); No",
+            + "[Fast Track, CW, Review Message](/cases/case-details/${[CASE_REFERENCE]}#Messages); No",
         "reviewMessageLA; Small Claim;; "
-            + "[Small Claim, LA, Review Message](/cases/case-details/${[CASE_REFERENCE]}#messagesTab); Yes",
+            + "[Small Claim, LA, Review Message](/cases/case-details/${[CASE_REFERENCE]}#Messages); Yes",
         "reviewMessageLA; Fast Track;; "
-            + "[Fast Track, LA, Review Message](/cases/case-details/${[CASE_REFERENCE]}#messagesTab); No",
+            + "[Fast Track, LA, Review Message](/cases/case-details/${[CASE_REFERENCE]}#Messages); No",
         "reviewMessageJudicial; Small Claim; CJ; "
-            + "[Small Claim, CJ, Review Message](/cases/case-details/${[CASE_REFERENCE]}#messagesTab); Yes",
+            + "[Small Claim, CJ, Review Message](/cases/case-details/${[CASE_REFERENCE]}#Messages); Yes",
         "reviewMessageJudicial; Fast Track; CJ; "
-            + "[Fast Track, CJ, Review Message](/cases/case-details/${[CASE_REFERENCE]}#messagesTab); No",
+            + "[Fast Track, CJ, Review Message](/cases/case-details/${[CASE_REFERENCE]}#Messages); No",
         "reviewMessageJudicial; Small Claim; DJ; "
-            + "[Small Claim, DJ, Review Message](/cases/case-details/${[CASE_REFERENCE]}#messagesTab); Yes",
+            + "[Small Claim, DJ, Review Message](/cases/case-details/${[CASE_REFERENCE]}#Messages); Yes",
         "reviewMessageJudicial; Fast Track; DJ; "
-            + "[Fast Track, DJ, Review Message](/cases/case-details/${[CASE_REFERENCE]}#messagesTab); No",
+            + "[Fast Track, DJ, Review Message](/cases/case-details/${[CASE_REFERENCE]}#Messages); No",
         "reviewMessageJudicial; Small Claim; Judge; "
-            + "[Small Claim, Judge, Review Message](/cases/case-details/${[CASE_REFERENCE]}#messagesTab); Yes",
+            + "[Small Claim, Judge, Review Message](/cases/case-details/${[CASE_REFERENCE]}#Messages); Yes",
         "reviewMessageJudicial; Fast Track; Judge; "
-            + "[Fast Track, Judge, Review Message](/cases/case-details/${[CASE_REFERENCE]}#messagesTab); No"
+            + "[Fast Track, Judge, Review Message](/cases/case-details/${[CASE_REFERENCE]}#Messages); No"
         }, delimiter = ';')
     void when_reviewMessage_then_return_allocatedTrackAndDescription(
         String taskType, String allocatedTrack,
