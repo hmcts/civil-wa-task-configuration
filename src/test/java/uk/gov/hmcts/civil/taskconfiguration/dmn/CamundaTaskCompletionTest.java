435067832a1b8dcd583f6c80f9b9eaa38a34b879
meulendijksm <max.meulendijks@hmcts.net>
2024-12-13 11:16:46 +0000

    CIV-15393 - remove completion changes

--- src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskCompletionTest.java
@@ -521,6 +521,6 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
 
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(56));
+        assertThat(logic.getRules().size(), is(52));
     }
 }
