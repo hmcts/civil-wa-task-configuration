880448fced7e938027e234d5ea9a149b82a2a27b
EllisD-B <ellis.domengerboyce@hmcts.net>
2025-02-18 13:50:14 +0000

    Fix missing task type

--- src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskTypesTest.java
+++ src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskTypesTest.java
@@ -372,6 +372,10 @@ class CamundaTaskTypesTest extends DmnDecisionTableBaseUnitTest {
             Map.of(
                 "taskTypeId", "queryManagementRespondToQuery",
                 "taskTypeName", "Respond to a Query"
+            ),
+            Map.of(
+                "taskTypeId", "LegalAdvisorRevisitApplication",
+                "taskTypeName", "Legal Advisor Revisit Application"
             )
         );
 
@@ -389,7 +393,7 @@ class CamundaTaskTypesTest extends DmnDecisionTableBaseUnitTest {
 
         DmnDecisionTableResult dmnDecisionTableResult = evaluateDmnTable(inputVariables);
 
-        assertThat(dmnDecisionTableResult.getResultList().size(), is(84));
+        assertThat(dmnDecisionTableResult.getResultList().size(), is(85));
     }
 
 }
