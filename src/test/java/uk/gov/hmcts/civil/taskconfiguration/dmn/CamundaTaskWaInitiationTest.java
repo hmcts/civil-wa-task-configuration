e5bc3ff402d6ca840970f3a9ab813d3589bd1e2c
jeswanth <jeswanth.reddy@hmcts.net>
2025-05-22 16:18:28 +0100

    CIV-16859 delay notification and visibility of the docs

--- src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaInitiationTest.java
+++ src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaInitiationTest.java
@@ -2277,7 +2277,7 @@ class CamundaTaskWaInitiationTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(282));
+        assertThat(logic.getRules().size(), is(283));
     }
 
     @ParameterizedTest
@@ -4048,6 +4048,25 @@ class CamundaTaskWaInitiationTest extends DmnDecisionTableBaseUnitTest {
         assertThat(workTypeResultList
                        .get(0).get("taskId"), is("uploadTranslatedOrderDocument"));
         assertThat(workTypeResultList.get(0).get("processCategories"), is("requestTranslation"));
+    }
+
+    @Test
+    void shouldCreateWATaskForWelshTranslationForDiscontinuanceTask() {
+        Map<String, Object> data = new HashMap<>();
+        data.put("featureToggleWA", "CUI_WELSH");
+        data.put("preTranslationDocumentType", "NOTICE_OF_DISCONTINUANCE");
+        Map<String, Object> caseData = new HashMap<>();
+        caseData.put("Data", data);
+        VariableMap inputVariables = new VariableMapImpl();
+
+        inputVariables.putValue("eventId", "GEN_NOTICE_OF_DISCONTINUANCE");
+        inputVariables.putValue("additionalData", caseData);
+        DmnDecisionTableResult dmnDecisionTableResult = evaluateDmnTable(inputVariables);
+
+        List<Map<String, Object>> workTypeResultList = dmnDecisionTableResult.getResultList();
+        assertThat(workTypeResultList.size(), is(1));
+        assertThat(workTypeResultList.get(0).get("name"), is("Upload Translated Notice Of Discontinuance"));
+        assertThat(workTypeResultList.get(0).get("taskId"), is("uploadTranslatedOrderDocument"));
     }
 
     @ParameterizedTest
