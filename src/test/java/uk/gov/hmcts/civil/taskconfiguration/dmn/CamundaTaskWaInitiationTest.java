cf2def802ee7857e060a84954bcec976b76f53dd
drummondjm <mark.drummond@version.com>
2025-06-23 10:13:02 +0100

    rebasing

--- src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaInitiationTest.java
+++ src/test/java/uk/gov/hmcts/civil/taskconfiguration/dmn/CamundaTaskWaInitiationTest.java
@@ -1807,7 +1807,7 @@ class CamundaTaskWaInitiationTest extends DmnDecisionTableBaseUnitTest {
         "JUDICIAL_DISTRICT, reviewMessageJudicial",
         "JUDICIAL_CIRCUIT, reviewMessageJudicial",
         "LEGAL_OPERATIONS, reviewMessageLA",
-        "ADMIN, reviewMessageCW"
+        "WLU_ADMIN, reviewMessageWLU",
     })
     void given_input_rolePool_should_return_correct_review_message_task(String rolePool, String expectedTaskId) {
         Map<String, Object> data = new HashMap<>();
@@ -2268,7 +2268,7 @@ class CamundaTaskWaInitiationTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(268));
+        assertThat(logic.getRules().size(), is(269));
     }
 
     @ParameterizedTest
