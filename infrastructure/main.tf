d33ad791179d4b1214da250811c863b67160220a
rishisharma <rishi.sharma@hmcts.net>
2026-07-10 14:14:08 +0100

    patch 1

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,34 @@
 provider "azurerm" {
   features {}
 }
+
+data "azuread_group" "dts_civil" {
+  display_name     = "DTS Civil"
+  security_enabled = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.product}-${var.component}-${var.env}"
+  location = var.location
+
+  tags = var.common_tags
+}
+
+data "azurerm_user_assigned_identity" "jenkins" {
+  name                = "jenkins-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
+}
+
+module "key-vault" {
+  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=DTSPO-31965/remove-jenkins-ptl-access"
+  name                    = "${var.product}-${var.env}"
+  product                 = var.product
+  env                     = var.env
+  tenant_id               = var.tenant_id
+  object_id               = var.jenkins_AAD_objectId
+  resource_group_name     = azurerm_resource_group.rg.name
+  product_group_object_id = data.azuread_group.dts_civil.object_id
+  jenkins_object_id       = data.azurerm_user_assigned_identity.jenkins.principal_id
+  common_tags             = var.common_tags
+  create_managed_identity = true
+}
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -6,6 +6,14 @@ variable "location" {
   default = "UK South"
 }
 
+variable "tenant_id" {
+  description = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environment variables and not normally required to be specified."
+}
+
+variable "jenkins_AAD_objectId" {
+  description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+}
+
 variable "env" {}
 
 variable "subscription" {}
