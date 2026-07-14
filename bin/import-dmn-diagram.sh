df05975fad41ed6b8a9d0d2b9f1d01f92c18c501
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2026-07-14 09:26:31 +0100

    Update import-dmn-diagram.sh

--- bin/import-dmn-diagram.sh
+++ bin/import-dmn-diagram.sh
@@ -8,6 +8,10 @@ product=${4}
 
 s2sSecret=${S2S_SECRET:-AABBCCDDEEFFGGHH}
 
+if [[ "${env}" == 'prod' ]]; then
+  s2sSecret=${S2S_SECRET_PROD}
+fi
+
 serviceToken=$($(realpath $workspace)/bin/shared/idam-lease-service-token.sh civil_service \
   $(docker run --rm toolbelt/oathtool --totp -b ${s2sSecret}))
 
